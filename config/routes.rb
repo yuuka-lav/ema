9b7f4e6b2929d43c082cd5f6f5cd686277193da7
iris5115 <yukurio5115iris@gmail.com>
2020-03-06 13:04:53 +0900

    create-sell-view
    商品出品画面フロント実装。

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'items#index'
-  resources :items, only: :show do
+  resources :items, only: [:show, :new] do
     collection do
       get 'confirm'
     end

14b95b492e0a366e2cef31071af40c0d35fbff7b
yuuka-lav <60505747+yuuka-lav@users.noreply.github.com>
2020-03-06 19:00:04 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name,:first_name,:first_name_yomi,:last_name,:last_name_yomi,:birthday,:prefecture,:city,:address,:zipcode, presence: true
+
 end

983e982aa823909d20c33600459fb6c125d1c36d
izumi <kizumi916@gmail.com>
2020-03-09 15:13:54 +0900

    item-display

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,10 @@
 class ItemsController < ApplicationController
   def index
-    @items = Item.all
+    @items = Item.all.order("created_at DESC").page.per(3)
     @images = Image.all
   end
   def show
+    @item = Item.find(params[:id])
   end
   def new
   end

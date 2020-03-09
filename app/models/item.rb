f6219a876cc4200a585037f542aa86bba939bd6f
iris5115 <yukurio5115iris@gmail.com>
2020-03-10 02:57:01 +0900

    edit image save error
    画像保存できないエラーを修正。

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :user,optional: true
   has_many :images
-  # mount_uploader :image, ImageUploader
-  belongs_to :category
+  accepts_nested_attributes_for :images
+  belongs_to :category,optional: true
 end

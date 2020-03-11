fc8fba9ac9ea959e833606d3b9ba875090db0b8b
izumi <kizumi916@gmail.com>
2020-03-11 21:11:12 +0900

    pay-jp-path

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  
+  require 'payjp'
   def index
     @items = Item.all.order("created_at DESC").page.per(3)
     @images = Image.all
@@ -20,6 +22,38 @@ class ItemsController < ApplicationController
       redirect_to new_item_path
     end
       
+  end
+
+  def confirm
+    @item = Item.find(params[:id])
+    @card = Card.where(user_id: current_user.id).first
+    
+    if @card.blank?
+     
+      redirect_to controller: "card", action: "new"
+    else
+      Payjp.api_key = Rails.application.credentials.PAYJP[:PAYJP_PRIVATE_KEY]
+      
+      customer = Payjp::Customer.retrieve(@card.customer_id)
+      
+      @default_card_information = customer.cards.retrieve(@card.card_id)
+    end
+  end
+
+  def pay
+    @item = Item.find(params[:id])
+    @card = Card.where(user_id: current_user.id).first
+    Payjp.api_key = Rails.application.credentials.PAYJP[:PAYJP_PRIVATE_KEY]
+    Payjp::Charge.create(
+    :amount => 13500,
+    :customer => @card.customer_id,
+    :currency => 'jpy',
+  )
+  redirect_to action: 'done' 
+  end
+
+  def done
+    @item = Item.find(params[:id])
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,19 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'items#index'
-  resources :items, only: [:index, :new, :create, :show] 
+  resources :items, only: [:index, :new, :create, :show] do
+    member do
+      get 'confirm', to: 'items#confirm'
+      post 'pay', to: 'items#pay'
+      get 'done', to: 'items#done'
+    end
+  end
   resources :users, only: :show 
   resources :card, only: [:new, :show] do
     collection do
       post 'show', to: 'card#show'
       post 'pay', to: 'card#pay'
       post 'delete', to: 'card#delete'
-    end
-  end
-  resources :purchase, only: :index do
-    collection do
-      get 'index', to: 'purchase#index'
-      post 'pay', to: 'purchase#pay'
-      get 'done', to: 'purchase#done'
     end
   end
 end

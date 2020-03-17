3d6545629052da5b70baaa1f4a5c4d7e8ed7eb57
iris5115 <yukurio5115iris@gmail.com>
2020-03-17 12:08:34 +0900

    edit item_spec.rb
    単体テストのコード修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -3,16 +3,19 @@ require 'rails_helper'
 describe Item do
   describe '#create' do
 
+    it "is valid with a name, info, price, condition_id, deliverydate_id, deliverypays_id, prefecture_id, category_id, brand" do
+      user = build(:user)
+      expect(user).to be_valid
+    end
 
-    # 名前が空であると保存できない
     it "is invalid without a name" do
       item = build(:item, name: "")
       item.valid?
       expect(item.errors[:name]).to include("を入力してください")
     end
 
-    it "is invalid without a info" do
-      item = build(:item, info: "")
+    it 'is invalid without a info' do
+      item = build(:item, text:"")
       item.valid?
       expect(item.errors[:info]).to include("を入力してください")
     end
@@ -23,40 +26,40 @@ describe Item do
       expect(item.errors[:price]).to include("を入力してください")
     end
 
-    it "is invalid without a brand" do
-      item = build(:item, brand: "")
-      item.valid?
-      expect(item.errors[:brand]).to include("can't be blank")
-    end
-
     it "is invalid without a condition_id" do
       item = build(:item, condition_id: "")
       item.valid?
-      expect(item.errors[:condition_id]).to include("can't be blank")
+      expect(item.errors[:condition_id]).to include("を入力してください")
     end
 
     it "is invalid without a deliverydate_id" do
       item = build(:item, deliverydate_id: "")
       item.valid?
-      expect(item.errors[:deliverydate_id]).to include("can't be blank")
+      expect(item.errors[:deliverydate_id]).to include("を入力してください")
     end
 
     it "is invalid without a deliverypays_id" do
       item = build(:item, deliverypays_id: "")
       item.valid?
-      expect(item.errors[:deliverypays_id]).to include("can't be blank")
+      expect(item.errors[:deliverypays_id]).to include("を入力してください")
     end
 
-    it "is invalid without a prefecture_id " do
-      item = build(:item, prefecture_id: "")
+    it "is invalid without a prefecture_id" do
+      item = build(:item, prefecture_id: nil)
       item.valid?
-      expect(item.errors[:prefecture_id]).to include("can't be blank")
+      expect(item.errors[:prefecture_id]).to include("を入力してください")
     end
 
-    it "is invalid without a category_id " do
-      item = build(:item, category_id: "")
+    it "is invalid without a category_id" do
+      item = build(:item, category_id: category.id)
       item.valid?
-      expect(item.errors[:category_id]).to include("can't be blank")
+      expect(item.errors[:category_id]).to include("を入力してください")
+    end
+
+    it "is invalid without a brand" do
+      item = build(:item, brand: nil)
+      item.valid?
+      expect(item.errors[:brand]).to include("を入力してください")
     end
 
   end

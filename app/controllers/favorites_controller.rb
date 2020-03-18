f855aca178c338f71abd23e580a2b210276272ff
ix4eight <ix4eight@gmail.com>
2020-03-18 16:27:44 +0900

    fav update

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,7 +2,7 @@ class FavoritesController < ApplicationController
   def create
     user=current_user
     item=Item.find(params[:item_id])
-    if Favorite.create(user_id: user.id,item_id:item.id)
+    if Favorite.create(user_id:user.id,item_id:item.id)
     redirect_to item
     else
       redirect_to root_url
@@ -13,7 +13,7 @@ class FavoritesController < ApplicationController
   def destroy
     user=current_user
     item=Item.find(params[:item_id])
-    if favorite=Favorite.find_by(user_id: user.id,item_id:item.id)
+    if favorite=Favorite.find_by(user_id: user.id,item_id:item.id) && favorite.delete
       favorite.delete
       redirect_to item_path(current_user)
     else

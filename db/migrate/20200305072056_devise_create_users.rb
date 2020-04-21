741474ef2f937adacb3d41e9f33b91bfcd7f45ca
yuuka-lav <60505747+yuuka-lav@users.noreply.github.com>
2020-04-21 15:39:19 +0900

    Update 20200305072056_devise_create_users.rb

--- db/migrate/20200305072056_devise_create_users.rb
+++ db/migrate/20200305072056_devise_create_users.rb
@@ -18,7 +18,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       t.integer :zipcode,           null: false
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-
+      
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
